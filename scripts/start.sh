aaa9e234b835d6833f70eaa4e9447b75a638629d
icyLabsDev <5783973+icylabsDev@users.noreply.github.com>
2018-11-05 08:45:38 +0100

    set SSID to 'videquus-camera-(last4MACADDRESS)'

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,13 +14,30 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 # wget --spider http://google.com 2>&1
 
 # 4. Is there an active WiFi connection?
-iwgetid -r
+# iwgetid -r
 
-if [ $? -eq 0 ]; then
-    printf 'Skipping WiFi Connect\n'
-else
-    printf 'Starting WiFi Connect\n'
-    ./wifi-connect
-fi
+# if [ $? -eq 0 ]; then
+#     printf 'Skipping WiFi Connect\n'
+# else
+#     printf 'Starting WiFi Connect\n'
+#     ./wifi-connect
+# fi
 
 # Start your application here.
+
+iface=$1
+
+  last4_mac=`cat /sys/class/net/$iface/address | sed 's/://g'`
+  last4_mac=`echo $last4_mac | tail -c 5`
+  ssid="videquus-camera-$last4_mac"
+  # psk="device$last4_mac"
+
+  iwgetid -r
+
+  if [ $? -eq 0 ]; then
+    echo "[STARTUP] WiFi already connected to: $(iwgetid -r)" > /dev/console
+  else
+    echo "[STARTUP] Starting WiFi AP" > /dev/console
+    # /usr/src/app/wifi/wifi-connect -i $iface -s $ssid -p $psk -u /usr/src/app/wifi/ui > /dev/console &
+    /usr/src/app/wifi/wifi-connect -i $iface -s $ssid -u /usr/src/app/wifi/ui > /dev/console &
+  fi
